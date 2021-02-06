24217448000fe5faf319f51c4d5937294dc25a19
Lukasz <bazyl603@gmail.com>
2021-02-06 21:39:00 +0100

    configurate react project and set router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy} from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import "./App.css";
+const Render = lazy(() => import('./components/Render'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router basename="/">
+        <div className="App">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>            
+              <Route path="/" exact component={Render} />
+              <Route exact render={() => <h1>404</h1>} />
+            </Switch>  
+          </Suspense>      
+        </div>
+      </Router>   
   );
 }
 
---
+++ src/components/Render.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import "./Render.css";
+
+function Render() {
+    const [month, setMonth] = useState('January');
+    console.log(month);
+
+    const selectMonth = (event) => {
+        setMonth(event.target.value);
+    }
+
+    return (
+        <div className="Render">
+            <h2 className="year">2020</h2>
+            <label>
+                Please select a month:
+                <br/>
+            <select className="select" value={month} onChange={selectMonth}>
+                <option value="January">January</option>
+                <option value="February">February</option>
+                <option value="March">March</option>
+                <option value="April">April</option>
+                <option value="May">May</option>
+                <option value="June">June</option>
+                <option value="July">July</option>
+                <option value="August">August</option>
+                <option value="September">September</option>
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="December">December</option>
+            </select>
+            <br/>
+        </label>
+            <button class="pure-button pure-button-primary"><Link to={{pathname: {month}}} className="text-button">Gooo</Link></button>
+        </div>
+    );
+}
+
+export default Render;
